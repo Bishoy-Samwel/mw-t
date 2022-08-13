256bd897add27716b79e2e3473bb442eebfe952b
Bishoy <bishoy.samwuel@gmail.com>
2022-08-13 19:30:00 +0200

    Initial Commit

---
+++ src/js/Main.js
@@ -0,0 +1,7 @@
+import * as ReactDOM from 'react-dom/client';
+import App from './App';
+
+const app = document.getElementById('root');
+
+const root = ReactDOM.createRoot(app);
+root.render(<App />);
